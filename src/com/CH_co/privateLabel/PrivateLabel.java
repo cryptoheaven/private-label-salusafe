444eed366a2031c99250e8ea5eae1ae97a00c0d3
Marcin <Marcin@Marcin7-PC>
2013-11-13 20:21:02 -0500

    minor -header adjustments

--- src/com/CH_co/privateLabel/PrivateLabel.java
+++ src/com/CH_co/privateLabel/PrivateLabel.java
@@ -1,36 +1,26 @@
-/*
-* Copyright 2001-2012 by CryptoHeaven Corp.,
-* Mississauga, Ontario, Canada.
-* All rights reserved.
-*
-* This software is the confidential and proprietary information
-* of CryptoHeaven Corp. ("Confidential Information").  You
-* shall not disclose such Confidential Information and shall use
-* it only in accordance with the terms of the license agreement
-* you entered into with CryptoHeaven Corp.
-*/
-
+/**
+ * Copyright 2001-2013 CryptoHeaven Corp. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of CryptoHeaven Corp. ("Confidential Information").  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with CryptoHeaven Corp.
+ */
 package com.CH_co.privateLabel;
 
 import com.CH_co.util.*;
 import java.net.URL;
 
-/**
-* <b>Copyright</b> &copy; 2001-2012
-* <a href="http://www.CryptoHeaven.com/DevelopmentTeam/">
-* CryptoHeaven Corp.
-* </a><br>All rights reserved.<p>
-*
-* Class Description:
-*
-*
-* Class Details:
-*
-*
-* <b>$Revision: 1.0 $</b>
-* @author  Marcin Kurzawa
-* @version
-*/
+/** 
+ * Copyright 2001-2013 CryptoHeaven Corp. All Rights Reserved.
+ *
+ * Class Description:
+ *
+ * <b>$Revision: $</b>
+ *
+ * @author  Marcin Kurzawa
+ */
 public class PrivateLabel implements PrivateLabelI {
 
   /**
